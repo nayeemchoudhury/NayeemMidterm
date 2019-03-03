5509fc7cef86d9b2ebaaf4bede6f2fe582ac25c9
nayeemchoudhury <nayeem.choudhury12@gmail.com>
2019-03-02 23:57:19 -0500

    first commit

---
+++ MidtermJanuary2019/src/math/problems/Pattern.java
@@ -0,0 +1,26 @@
+package math.problems;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		/* Read this numbers, find the pattern then implement the logic from this pattern.which will give you this output.
+		 * 100,99,98,97,96,95,94,93,92,91,90,88,86,84,82,80,78,76,74,72,70,67,64,61,58,55,52,49,46,43,40,36,32............
+		 *
+		 *
+		 */
+		int start = 100;
+		for (int x=0; x<=100; x++){
+			if (start<=100&&start>90){System.out.print(start+",");
+			start--;}
+			else if (start<=90&&start>=80){System.out.print(start+",");
+				start = (start -2);}
+			else{
+				System.out.println("blahh");
+			}
+
+		}
+
+
+	}
+
+}
