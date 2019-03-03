c24fe4e4967c70779fae67294ec97815e453c86a
nayeemchoudhury <nayeem.choudhury12@gmail.com>
2019-03-03 13:33:00 -0500

    second commit

--- MidtermJanuary2019/src/math/problems/MakePyramid.java
+++ MidtermJanuary2019/src/math/problems/MakePyramid.java
@@ -5,11 +5,11 @@ public class MakePyramid {
 
         /*   Implement a large Pyramid of stars in the screen with java.
 
-         *
-         * *
-         * * *
-         * * * *
-         * * * * *
+              *
+             * *
+            * * *
+           * * * *
+          * * * * *
          * * * * * *
 
          */
@@ -18,11 +18,11 @@ public class MakePyramid {
         String space = " ";
         //loop that decides how many lines------------
         for (int x = 0; x < numRows; x++) {
-            //spaces
+            //inner loop that decides spaces
             for (int y = numRows - x; y > 1; y--) {
                 System.out.print(space);
             }
-            //stars
+            //inner loop that decides stars
             for (int z = 0; z < x; z++) {
                 System.out.print("* ");
             }
